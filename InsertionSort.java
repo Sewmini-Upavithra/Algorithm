140ff7c6af766ab6deaf8f058aaf035e4174ee23
Sewmini-Upavithra <sew.upavithra@gmail.com>
2024-06-02 20:49:55 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,48 @@
+package sorting;
+
+public class InsertionSort {
+
+	 public static void insertionSort(int[] arr) {
+		// TODO Auto-generated method stub
+		
+		int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements of array[0..i-1], that are greater than key,
+            // to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    // Method to print the elements of the array
+    public static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // Main method to test the Insertion Sort
+    public static void main(String[] args) {
+        int[] arr = {26, 56, 48, 31, 62};
+
+        System.out.println("Unsorted array:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+
+		
+
+}
+	
